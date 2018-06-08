19be2bd013715bcdd09defeb2465a5562b4155c9
Ricardo CF <ri_ca_fa@hotmail.com>
2018-06-07 23:04:17 -0300

    add sass compiler and create table migrations

---
+++ database/migrations/2018_06_08_015732_create_schedules_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patient_id');
+            $table->unsignedInteger('doctor_id');
+            $table->datetime('start_at');
+            $table->datetime('end_at');
+            $table->boolean('showed_up');
+            $table->string('reason');
+            $table->string('obs');
+            $table->timestamps();
+            
+            #Foreign Key
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('patients','PatientController');
+Route::resource('doctors','DoctorController');
+Route::resource('schedules','ScheduleController');
